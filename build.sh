98a4e368d75eaea765cec22414993f1fa6ee2240
Carsten Behring <carsten.behring@gmail.com>
2013-11-03 17:17:18 +0100

    added integration tests and build script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 lsof -n -i:8080 | grep LISTEN | awk '{ print $2 }' | uniq | xargs -r kill -9
 lsof -n -i:4444 | grep LISTEN | awk '{ print $2 }' | uniq | xargs -r kill -9
-karma start
 rm -rf target
-lein.sh do clean, midje, install
+lein karma
+lein do clean, midje, install
 ./selenium/start &
-lein.sh ring server-headless 8080  &
+lein ring server-headless 8080  &
 sleep 10
-lein.sh shell protractor resources/protractor_conf.js
+lein shell protractor resources/protractor_conf.js
--- project.clj
+++ project.clj
@@ -19,9 +19,14 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {
-                    :dependencies [[midje "1.5.1"][clj-ns-browser "1.3.1"]
+                    :dependencies [[midje "1.5.1"]
+                                   [clj-ns-browser "1.3.1"]
                                    ]
-                    :plugins [[lein-midje "3.1.1"][lein-ancient "0.5.2"][lein-shell "0.3.0"] [lein-ring "0.8.7"]
+                    :plugins [[lein-midje "3.1.1"]
+                              [lein-ancient "0.5.2"]
+                              [lein-shell "0.3.0"]
+                              [lein-ring "0.8.7"]
+                              [lein-karma "0.1.0"]
                               ]}
              }
   :ring {:handler todo.web/app}
